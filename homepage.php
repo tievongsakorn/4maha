7c4f4834f44c1fc821d5563e6b1bdd19c318e973
tievongsakorn <vongsakorntie@gmail.com>
2020-11-22 22:36:44 +0700

    First update

--- homepage.php
+++ homepage.php
@@ -1,1 +1,12 @@
-<h1>hello</h1>
+<div class="container">
+    <h1 style="margin-top : 1em">
+        เว็บแอปพลิเคชันแนะนำเล่นทางการท่องเที่ยว 4 มหาราชจังหวัดตาก
+    </h1>
+    <div class="main-carousel" data-flickity='{ "percentPosition": false ,"wrapAround": true , "autoPlay": 2500 ,"pageDots": false, "fade": true ,"fullscreen": true}'>
+                <img class="carousel-cell" src="img/1.jpg" alt="" />
+                <img class="carousel-cell" src="img/1.jpg" alt="" />
+                <img class="carousel-cell" src="img/1.jpg" alt="" />
+                <img class="carousel-cell" src="img/1.jpg" alt="" />
+                <img class="carousel-cell" src="img/1.jpg" alt="" />
+    </div>
+</div>
--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
@@ -53,8 +55,17 @@ else{
 	require_once("homepage.php");
 }
 ?>
+
+<footer>
+  <div class="copy">
+    <div class="h5">
+      Copyright Vongsakorn Ketsuriyong Since 2020
+    </div>
+  </div>
+</footer>
 </body>
 </html>
+<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
